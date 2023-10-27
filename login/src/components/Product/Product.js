919968f704047b2b905a97d74b15d6a15d311e8f
mlafrat <146100054+mlafrat@users.noreply.github.com>
2023-10-26 23:09:10 -0400

    Uploaded React App

---
+++ login/src/components/Product/Product.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Product.css';
+
+export default class Product extends Component {
+  render() {
+    return(
+      <div className="wrapper">
+        <div>
+          Shopping Cart: 0 total items.
+        </div>
+        <div>Total: 0</div>
+
+        <div className="product"><span role="img" aria-label="ice 
+cream">🍦</span></div>
+        <button>Add</button> <button>Remove</button>
+      </div>
+    )
+  }
+}
