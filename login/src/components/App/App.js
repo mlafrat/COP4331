e50cc01e4d3de33de121686ffc309c3a952486e0
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2023-10-28 20:38:54 -0400

    redirect works!

--- login/src/components/App/App.js
+++ login/src/components/App/App.js
@@ -1,32 +1,31 @@
-import React , { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
 import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
 
 function App() {
-  const [token, setToken] = useState();
+    const [token, setToken] = useState();
 
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
-
-  return (
-    <div className="wrapper">
-      <h1>Application</h1>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <h1>Application</h1>
+            <Router>
+                <Switch>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/preferences">
+                        <Preferences />
+                    </Route>
+                    <Route path="/">
+                        <Login setToken={setToken} />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
