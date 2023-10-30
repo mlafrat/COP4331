ddb03d7c8340b1ce5e4d2d4090ffebc57ba09363
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2023-10-30 12:46:48 -0400

    Update App.js

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -6,6 +6,7 @@ import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences';
 
 function App() {
+    // eslint-disable-next-line
     const [token, setToken] = useState();
 
     return (
