ff97daf55b346224f440998403c2fb13a40a3775
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2023-11-01 19:37:41 -0400

    register good
    also adjusted some spacing

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,65 +1,69 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
 import './Login.css';
 
-export default function Login({ setToken }) {
-  const [formData, setFormData] = useState({
-    username: '',
-    password: ''
-  });
-
-  const history = useHistory();
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const response = await fetch('http://localhost:3001/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData)
+export default function Login({setToken}) {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
     });
 
-    if (response.ok) {
-      const contentType = response.headers.get('content-type');
-      if (contentType && contentType.includes('application/json')) {
-        const data = await response.json();
-        setToken(data.token);
-        history.push('/dashboard');
-      } else {
-        console.log(await response.text());
-      }
-    } else {
-      console.error('Error:', response.statusText);
-    }
-  };
+    const history = useHistory();
 
-  return (
-      <div className="login-wrapper">
-        <h1>Please Log In</h1>
-        <form onSubmit={handleSubmit}>
-          <label>
-            <p>Username</p>
-            <input type="text" name="username" value={formData.username} onChange={handleChange} />
-          </label>
-          <label>
-            <p>Password</p>
-            <input type="password" name="password" value={formData.password} onChange={handleChange} />
-          </label>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-          <a href="http://localhost:3001/auth/google/callback">Login with Google</a>
-        </form>
-      </div>
-  );
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch('http://localhost:3001/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        });
+
+        if (response.ok) {
+            const contentType = response.headers.get('content-type');
+            if (contentType && contentType.includes('application/json')) {
+                const data = await response.json();
+                setToken(data.token);
+                history.push('/dashboard');
+            } else {
+                console.log(await response.text());
+            }
+        } else {
+            console.error('Error:', response.statusText);
+        }
+    };
+
+    return (
+        <div className="login-wrapper">
+            <h1>Please Log In</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <p>Username</p>
+                    <input type="text" name="username" value={formData.username} onChange={handleChange}/>
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" name="password" value={formData.password} onChange={handleChange}/>
+                </label>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+                <a href="http://localhost:3001/auth/google/callback">Login with Google</a>
+            </form>
+
+            <div className="register-button">
+                <button onClick={() => history.push('/register')}>Register</button>
+            </div>
+        </div>
+    );
 }
--- server/routes/login.js
+++ server/routes/login.js
@@ -2,20 +2,20 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
-module.exports = function(db) {
+module.exports = function (db) {
     // Define a route for handling login
     router.post("/", async (req, res) => {
-        const { username, password } = req.body;
+        const {username, password} = req.body;
 
         try {
-            const user = await db.collection("userData").findOne({ username });
+            const user = await db.collection("userData").findOne({username});
 
             if (user) {
                 const isPasswordValid = await bcrypt.compare(password, user.password);
 
                 if (isPasswordValid) {
                     res.setHeader('Content-Type', 'application/json'); // Set Content-Type header
-                    res.send(JSON.stringify({ message: 'Login successful' }));
+                    res.send(JSON.stringify({message: 'Login successful'}));
                 } else {
                     res.status(401).send("Invalid credentials");
                 }
