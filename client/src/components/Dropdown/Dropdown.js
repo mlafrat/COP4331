7d3dd9de03af481eae280c7ebd72f95f8b0b1244
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2023-11-07 16:50:32 -0500

    display username

--- client/src/components/Dropdown/Dropdown.js
+++ client/src/components/Dropdown/Dropdown.js
@@ -1,63 +1,58 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import user from './user.png';
 import edit from './edit.png';
 import inbox from './envelope.png';
 import logout from './log-out.png';
 import knightro from './knightro.png';
 import './Dropdown.css';
-import React, {useState, useEffect, useRef} from 'react';
-import {Link} from 'react-router-dom';
-
+import Cookies from 'js-cookie'; // Import js-cookie
 
 function Dropdown() {
-
-  // Ensure dropdown is closed until moused over.
   const [open, setOpen] = useState(false);
-
-  let dropRef = useRef();
+  const dropRef = useRef();
 
   useEffect(() => {
-    let handler = (e)=>{
-      if(!dropRef.current.contains(e.target)){
+    let handler = (e) => {
+      if (!dropRef.current.contains(e.target)) {
         setOpen(false);
-        console.log(dropRef.current);
-      }      
+      }
     };
 
-    // Added event listening functions to show mouse as a pointer when it's on the user's photo.
     document.addEventListener("mousedown", handler);
-    
 
-    return() =>{
+    return () => {
       document.removeEventListener("mousedown", handler);
-    }
+    };
+  }, []);
 
-  });
+  // Retrieve user data from cookie
+  const userData = Cookies.get('user') ? JSON.parse(Cookies.get('user')) : null;
+  const userName = userData ? userData.username : '';
 
-  // Shows each menu dropdown item with icons when the dropdown is open.
-  // User's photo is also shown initially on menu button.
   return (
-    <div className="Dropdown">
-      <div className='menu-container' ref={dropRef}>
-        <div className='menu-trigger' onClick={() => setOpen(!open)}>
-          <img src={knightro} alt="User Avatar" />
-        </div>
+      <div className="Dropdown">
+        <div className='menu-container' ref={dropRef}>
+          <div className='menu-trigger' onClick={() => setOpen(!open)}>
+            <img src={knightro} alt="User Avatar" />
+          </div>
 
-        <div className={`dropdown-menu ${open? 'active' : 'inactive'}`} >
-          <h3>Knightro<br/></h3>
-          <ul>
-            <DropdownItem img = {user} text = {"Profile Settings"}/>
-            <DropdownItem img = {edit} text = {"My Reviews"}/>
-            <DropdownItem img = {inbox} text = {"Found a Microwave?"}/>
-            <li className='dropdownItem'>
-              <Link to="/login">
-                <img src={logout} alt="Logout" />
-                Logout
-              </Link>
-            </li>
-          </ul>
+          <div className={`dropdown-menu ${open ? 'active' : 'inactive'}`} >
+            <h3>{userName}<br /></h3>
+            <ul>
+              <DropdownItem img={user} text={"Profile Settings"} />
+              <DropdownItem img={edit} text={"My Reviews"} />
+              <DropdownItem img={inbox} text={"Found a Microwave?"} />
+              <li className='dropdownItem'>
+                <Link to="/login">
+                  <img src={logout} alt="Logout" />
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
