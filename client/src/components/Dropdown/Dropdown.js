9ad94563d70f6504fdb990711c206e305b09d348
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2023-11-10 08:29:40 -0500

    found a microwave clickable
    added the link to /found-microwave to communicate with App.js so we can click on it!

--- client/src/components/Dropdown/Dropdown.js
+++ client/src/components/Dropdown/Dropdown.js
@@ -52,7 +52,12 @@ function Dropdown() {
                 </Link>
               </li>
               <DropdownItem img={edit} text={"My Reviews"} />
-              <DropdownItem img={inbox} text={"Found a Microwave?"} />
+              <li className='dropdownItem'>
+                <Link to="/found-microwave">
+                  <img src={inbox} alt="Found a Microwave?" />
+                  Found a Microwave?
+                </Link>
+              </li>
               <li className='dropdownItem'>
                 <Link to="/login" onClick={handleLogout}>
                   <img src={logout} alt="Logout" />
