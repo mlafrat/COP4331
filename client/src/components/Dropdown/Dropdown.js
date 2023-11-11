efed8d12ba4d3695b3ba1388e06efcdb83dbab2c
mlafrat <146100054+mlafrat@users.noreply.github.com>
2023-11-11 10:00:07 -0600

    Updated Routes

--- client/src/components/Dropdown/Dropdown.js
+++ client/src/components/Dropdown/Dropdown.js
@@ -1,83 +1,82 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
 import user from './user.png';
-import edit from './edit.png';
 import inbox from './envelope.png';
 import logout from './log-out.png';
 import knightro from './knightro.png';
 import './Dropdown.css';
-import Cookies from 'js-cookie';
+import React, {useState, useEffect, useRef} from 'react';
+import {Link} from 'react-router-dom';
+
 
 function Dropdown() {
+
+  // Ensure dropdown is closed until moused over.
   const [open, setOpen] = useState(false);
-  const dropRef = useRef();
+
+  let dropRef = useRef();
 
   useEffect(() => {
-    let handler = (e) => {
-      if (!dropRef.current.contains(e.target)) {
+    let handler = (e)=>{
+      if(!dropRef.current.contains(e.target)){
         setOpen(false);
-      }
+        console.log(dropRef.current);
+      }      
     };
 
+    // Added event listening functions to show mouse as a pointer when it's on the user's photo.
     document.addEventListener("mousedown", handler);
+    
 
-    return () => {
+    return() =>{
       document.removeEventListener("mousedown", handler);
-    };
-  }, []);
+    }
 
-  const handleLogout = () => {
-    // Remove the user cookie when logging out
-    Cookies.remove('user');
-  };
+  });
 
-  // Retrieve user data from cookie
-  const userData = Cookies.get('user') ? JSON.parse(Cookies.get('user')) : null;
-  const userName = userData ? userData.username : '';
-
+  // Shows each menu dropdown item with icons when the dropdown is open.
+  // User's photo is also shown initially on menu button.
   return (
-      <div className="Dropdown">
-        <div className='menu-container' ref={dropRef}>
-          <div className='menu-trigger' onClick={() => setOpen(!open)}>
-            <img src={knightro} alt="User Avatar" />
-          </div>
+    <div className="Dropdown">
+      <div className='menu-container' ref={dropRef}>
+        <div className='menu-trigger' onClick={()=>{setOpen(!open)}}>
+          <img src={knightro} alt="default avatar"></img>
+        </div>
 
-          <div className={`dropdown-menu ${open ? 'active' : 'inactive'}`} >
-            <h3>{userName}<br /></h3>
-            <ul>
-              <li className='dropdownItem'>
-                <Link to="/profile-settings">
-                  <img src={user} alt="Profile Settings" />
-                  Profile Settings
-                </Link>
-              </li>
-              <DropdownItem img={edit} text={"My Reviews"} />
-              <li className='dropdownItem'>
-                <Link to="/found-microwave">
-                  <img src={inbox} alt="Found a Microwave?" />
-                  Found a Microwave?
-                </Link>
-              </li>
-              <li className='dropdownItem'>
-                <Link to="/login" onClick={handleLogout}>
-                  <img src={logout} alt="Logout" />
-                  Logout
-                </Link>
-              </li>
-            </ul>
-          </div>
+        <div className={`dropdown-menu ${open? 'active' : 'inactive'}`} >
+          <h3>Knightro<br/></h3>
+          <ul>
+            <li className='dropdownItem'>
+              <Link to="/profile-settings">
+                <img src={user} alt="Profile Settings" />
+                Profile Settings
+              </Link>
+            </li>
+            <li className='dropdownItem'>
+              <Link to="/my-reviews">
+                <img src={inbox} alt="My Reviews" />
+                My Reviews
+              </Link>
+            </li>
+            <li className='dropdownItem'>
+              <Link to="/found-microwave">
+                <img src={inbox} alt="Found a Microwave?" />
+                Found a Microwave?
+              </Link>
+            </li>
+            <DropdownItem img = {logout} text = {"Logout"}/>
+          </ul>
         </div>
       </div>
+    </div>
   );
 }
 
 // Shows properties of each item in their formatting order using a list.
 function DropdownItem(props){
   return(
-      <li className = 'dropdownItem'>
-        <img src={props.img} alt={props.img}></img>
-        <div> {props.text} </div>
-      </li>
+    <li className = 'dropdownItem'>
+      <img src={props.img} alt="profile avatar"></img>
+      <txt> {props.text} </txt>
+    </li>
   );
 }
 
