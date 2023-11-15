7e0293e76a788c4bdc23549ac8321d475fe8eabd
Adam Whitlock <adamkwhitlock@gmail.com>
2023-11-15 15:06:23 -0500

    Added all input forms and links onto register page

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,92 +1,174 @@
-import React, {useState} from "react";
-import {useHistory} from "react-router-dom";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import "./Register.css";
+import { Box, Container, Link, Grid } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 export default function SignUp() {
-    const [formData, setFormData] = useState({
-        email: "",
-        username: "",
-        password: "",
-        confirmPassword: ""
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:3001/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
     });
 
-    const history = useHistory();
+    const data = await response.json();
 
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
+    if (response.ok) {
+      history.push("/login"); // Redirect to the dashboard
+    } else {
+      alert(data); // Error message from the server
+    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  return (
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Box component="form" onSubmit={handleSubmit}>
+          <Typography component="h1" variant="h5">
+            Register
+          </Typography>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+            value={formData.email}
+            onChange={handleChange}
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="off"
+            value={formData.username}
+            onChange={handleChange}
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="password"
+            label="Password"
+            name="password"
+            type="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="confirmPassword"
+            label="Confirm Password"
+            name="confirmPassword"
+            type="password"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 1 }}
+          >
+            Sign In
+          </Button>
+          <Grid Container>
+            <Grid item xs>
+              <Link href="/login" variant="body2">
+                {"Already have an account? Sign in here"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
 
-        const response = await fetch('http://localhost:3001/register', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(formData)
-        });
-
-        const data = await response.json();
-
-        if (response.ok) {
-            history.push("/login"); // Redirect to the dashboard
-        } else {
-            alert(data); // Error message from the server
-        }
-    };
-
-    return (
-        <div className="signup-wrapper">
-            <h1>Please Sign Up</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <p>Email</p>
-                    <input
-                        type="text"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                    />
-                </label>
-                <label>
-                    <p>Username</p>
-                    <input
-                        type="text"
-                        name="username"
-                        value={formData.username}
-                        onChange={handleChange}
-                    />
-                </label>
-                <label>
-                    <p>Password</p>
-                    <input
-                        type="password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                    />
-                </label>
-                <label>
-                    <p>Confirm Password</p>
-                    <input
-                        type="password"
-                        name="confirmPassword"
-                        value={formData.confirmPassword}
-                        onChange={handleChange}
-                    />
-                </label>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
-            <div className="login-button">
-                <button onClick={() => history.push('/login')}>Login</button>
-            </div>
+    /* <div className="signup-wrapper">
+        <h1>Please Sign Up</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <p>Email</p>
+            <input
+              type="text"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            <p>Username</p>
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            <p>Password</p>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            <p>Confirm Password</p>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+            />
+          </label>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+        <div className="login-button">
+          <button onClick={() => history.push("/login")}>Login</button>
         </div>
-    );
+      </div> */
+  );
 }
