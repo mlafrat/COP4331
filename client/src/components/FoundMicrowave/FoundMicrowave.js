c53b52b124d1feabab2049085c8aef7901b51b38
WhitlockAdam <adamkwhitlock@gmail.com>
2023-11-19 17:31:00 -0500

    Formated the foundmicrowave form

--- client/src/components/FoundMicrowave/FoundMicrowave.js
+++ client/src/components/FoundMicrowave/FoundMicrowave.js
@@ -1,91 +1,107 @@
-import React, { useState } from 'react';
-import './FoundMicrowave.css';
-import Button from '@material-ui/core/Button';
-import TextField from '@mui/material/TextField';
+import React, { useState } from "react";
+import "./FoundMicrowave.css";
+import Button from "@material-ui/core/Button";
+import TextField from "@mui/material/TextField";
 import Cookies from "js-cookie";
-
+import { Box, Container, Grid } from "@material-ui/core";
 
 function FoundMicrowave() {
+  const userData = Cookies.get("user") ? JSON.parse(Cookies.get("user")) : null;
+  const userId = userData ? userData.user_id : null;
 
-    const userData = Cookies.get('user') ? JSON.parse(Cookies.get('user')) : null;
-    const userId = userData ? userData.user_id : null;
+  const [formData, setFormData] = useState({
+    location_building: "",
+    location_description: "",
+  });
 
-    const [formData, setFormData] = useState({
-        location_building: '',
-        location_description: ''
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        });
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        try {
-            const response = await fetch(`http://localhost:3001/addMicrowave/${userId}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
-
-            if (response.ok) {
-                alert('Microwave added successfully');
-            } else {
-                const errorMessage = await response.text();
-                alert(`Error: ${errorMessage}`);
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            alert('Network error. Please try again.');
+    try {
+      const response = await fetch(
+        `http://localhost:3001/addMicrowave/${userId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
         }
-    };
-
-    return(
-        <div className="microwave-form-wrapper">
-            <h1>New Microwave</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <TextField
-                        id="location-building"
-                        label="Building Location"
-                        type="text"
-                        name="location_building"
-                        value={formData.location_building}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        id="location-description"
-                        label="Location Description"
-                        multiline
-                        rows={6}
-                        placeholder="Please provide a detailed description of where this microwave is located."
-                        type="text"
-                        name="location_description"
-                        value={formData.location_description}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <Button component="label" variant="contained">
-                            Upload a Photo of the Microwave Here!
-                            <input type="file" accept="image/*" style={{ display: 'none' }}/>
-                        </Button>
-                    </div>
-                <div>
-                    <Button type="submit" variant="contained">Submit For Review</Button>
-                </div>
-            </form>
-        </div>
       );
 
+      if (response.ok) {
+        alert("Microwave added successfully");
+      } else {
+        const errorMessage = await response.text();
+        alert(`Error: ${errorMessage}`);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Network error. Please try again.");
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="md">
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <h1>Have a new microwave?</h1>
+        <TextField
+          margin="small"
+          id="location-building"
+          fullWidth
+          label="Building Location"
+          placeholder="Tell us what building this microwave is located in"
+          type="text"
+          name="location_building"
+          value={formData.location_building}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          id="location-description"
+          label="Location Description"
+          multiline
+          rows={6}
+          placeholder="Please provide a detailed description of where this microwave is located"
+          type="text"
+          name="location_description"
+          value={formData.location_description}
+          onChange={handleChange}
+        />
+        <Grid container>
+          <Grid item xs>
+            <Button component="label" variant="contained">
+              Upload Photo Here!
+              <input type="file" accept="image/*" style={{ display: "none" }} />
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button type="submit" variant="contained" color="secondary">
+              Submit For Review
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
 }
 
 export default FoundMicrowave;
