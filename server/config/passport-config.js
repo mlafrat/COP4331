2f682e78e0c9123babeef70669a523435a2321dd
Allison Turner <90711802+AllisonDT@users.noreply.github.com>
2023-11-19 21:51:25 -0500

    fixed google login pic

--- server/config/passport-config.js
+++ server/config/passport-config.js
@@ -2,8 +2,8 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: "798919910451-bapigtqppu7lb16s9rkcjp35gukpl1qk.apps.googleusercontent.com",
-    clientSecret: "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",
+    clientID: "798919910451-nigbdkijb3bbhu8qnri3lhm8s3rcp1p9.apps.googleusercontent.com",
+    clientSecret: "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",
     callbackURL: "/auth/google/callback",
     scope: ["profile", "email"]
 }, (accessToken, refreshToken, profile, done) => {
