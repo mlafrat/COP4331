f7a6049357b38268f37eb7ac0a006bdbf8f1ab60
WhitlockAdam <adamkwhitlock@gmail.com>
2023-11-19 22:43:04 -0500

    Added text boxes to the change password page

--- client/src/components/ChangePassword/ChangePassword.js
+++ client/src/components/ChangePassword/ChangePassword.js
@@ -1,42 +1,65 @@
-import './ChangePassword.css'
-import Button from '@material-ui/core/Button'
+import "./ChangePassword.css";
+import Button from "@material-ui/core/Button";
+import { Container, Box, TextField } from "@mui/material";
 
 function ChangePassword() {
-    return(
-        // Need to make sure current password is right, then change password/confirm change.
-        <div className="change-password-wrapper">
-            <h1>Change Password</h1>
-            <form>
-                <label>
-                    <p>Current Password</p>
-                    <input
-                        type="password"
-                        name="password"
-                    />
-                </label>
-                <label>
-                    <p>New Password</p>
-                    <input
-                        type="password"
-                        name="newPassword"
-                    />
-                </label>
-                <label>
-                    <p>Confirm New Password</p>
-                    <input
-                        type="password"
-                        name="confirmNewPassword"
-                    />
-                </label>
-                <div>
-                    <Button type="submit" variant="contained">
-                        Change Password
-                    </Button>
-                </div>
-            </form>
-        </div>
-
-    );
+  return (
+    // Need to make sure current password is right, then change password/confirm change.
+    <Container maxWidth="sm">
+      <h1>Change Password</h1>
+      <Box
+        component="form"
+        sx={{
+          marginTop: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="confirmPassword"
+          label="Confirm Password"
+          name="confirmPassword"
+          type="password"
+          // value={formData.confirmPassword}
+          // onChange={handleChange}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="newPassword"
+          label="New Password"
+          type="password"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="confirmNewPassword"
+          label="Confirm New Password"
+          type="password"
+        />
+        <Button type="submit" variant="contained">
+          Change Password
+        </Button>
+      </Box>
+    </Container>
+  );
 }
 
 export default ChangePassword;
