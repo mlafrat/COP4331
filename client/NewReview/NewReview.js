0bfe067b7e9c226be7996b10a790536290338d16
mlafrat <146100054+mlafrat@users.noreply.github.com>
2023-11-24 20:00:46 -0500

    added microwave styling stuff

---
+++ client/NewReview/NewReview.js
@@ -0,0 +1,185 @@
+import React, { useState, useEffect } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@mui/material/TextField';
+import Cookies from 'js-cookie';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+import MicrowaveIcon from '@mui/icons-material/Microwave';
+import "./NewReview.css";
+import { Container, Box, Stack } from "@mui/material";
+
+
+
+function NewReview() {
+    //get rating
+    const [value, setValue] = React.useState(2);
+
+    //get user id
+    const userData = Cookies.get('user') ? JSON.parse(Cookies.get('user')) : null;
+    const userId = userData ? userData.user_id : null;
+
+    //get microwave id
+    console.log("receiving microwave id:");
+    const microwave_id = window.location.hash.substring(1)
+    console.log(microwave_id);   
+
+    const [formData, setFormData] = useState({
+        review: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+     const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        //define new review to send
+        const newWave = {
+            review: formData.review,
+            rating: value, 
+            microwave_id: microwave_id,
+            user_id: userId
+        }
+
+        try {
+            const response = await fetch(`http://localhost:3001/addReview`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newWave),
+            });
+
+            if (response.ok) {
+                //return to previous page
+                window.location.href = `/test-reviews#${microwave_id}`;
+            } else {
+                const errorMessage = await response.text();
+                alert(`Error: ${errorMessage}`);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Network error. Please try again.');
+        }
+    };
+
+    //return to previous page
+    const handleClose = async () => {
+        window.location.href = `/test-reviews#${microwave_id}`;
+    };    
+
+    return (
+        <Container component="main" maxWidth="md">
+            <Box
+                component="form"
+                onSubmit={handleSubmit}
+                sx={{
+                  marginTop: 4,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  textAlign: "center",
+                }}
+            >
+                <div>
+                    <h1>New Review</h1>   
+                </div>
+                <div>
+                                <Rating
+                                    name="star-rating"
+                                    sx={{position:"relative"}}
+                                    value={value}
+                    
+                                    onChange={(event, newValue) => {
+                                    setValue(newValue);
+                                    }}
+                                    icon={<MicrowaveIcon fontSize="inherit" sx={{color:"black", alignItems:"center", position:"relative"}} />}
+                                    emptyIcon={<MicrowaveIcon fontSize="inherit" />}
+                                />
+                            </div>
+                <Typography component="legend">1 Star = Terrible, 5 Stars = Knightrolicious</Typography>
+                <div>
+                    <TextField
+                        id="review"
+                        label="Review"
+                        multiline
+                        margin="normal"
+                        required
+                        fullWidth
+                        sx={{
+                            "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+                                borderColor: "black"
+                            },
+                            "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+                                borderColor: "black"
+                            },
+                            "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                                borderColor: "black"
+                            },
+                            "& .MuiOutlinedInput-input": {
+                                color: "black"
+                            },
+                            "&:hover .MuiOutlinedInput-input": {
+                                color: "black"
+                            },
+                            "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input": {
+                                backgroundColor: "white",
+                                color: "black"
+                            },
+                            "& .MuiInputLabel-outlined": {
+                                color: "black"
+                            },
+                            "&:hover .MuiInputLabel-outlined": {
+                                color: "black"
+                            },
+                            "& .MuiInputLabel-outlined.Mui-focused": {
+                                color: "black"
+                            }
+                        }}
+                        rows={6}
+                        placeholder="Leave your review here!"
+                        type="text"
+                        name="review"
+                        value={formData.review}
+                        onChange={handleChange}
+                    />
+                </div>
+             
+                <div>
+                    <Stack sx={{ pt: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <Stack direction="row" spacing={4} justifyContent="center">
+                            <div>
+                                <Button               
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 1, position:"absolute" }}>
+                                        Submit Review
+                                </Button>
+                            </div>
+                            <div>
+                                <Button 
+                                    onClick={() => handleClose()} 
+                                    variant="contained" fullWidth
+                                    margin="normal"
+                                    sx={{ mt: 3, mb: 2, position:"absolute" }}>Cancel
+                                </Button>
+                            </div>
+                        </Stack>
+                    </Stack>
+                </div>
+            </Box>
+       </Container> 
+
+       
+    );
+}
+
+export default NewReview;
+
+
---
+++ client/TestReviews/TestReviews.js
@@ -0,0 +1,134 @@
+import React, {useState, useEffect} from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import { Box, Stack } from "@mui/material";
+import './TestReviews.css';
+
+function TestReviews() {
+
+    //get microwave id
+    const microwave_id = window.location.hash.substring(1)
+    console.log("viewing microwave:");
+    console.log(microwave_id);
+
+    //review stuff
+    const [reviews, setReviews] = useState([]);
+
+    const initializeReviews = (reviewData) => {
+        const microwaveReviews= reviewData.map((review) => ({
+            ...review,
+        }));
+        setReviews(microwaveReviews);
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            if (microwave_id) {
+                try {
+                    // Fetch call to get reviews by microwave ID
+                    const response = await fetch(`http://localhost:3001/viewMicrowaveReviews?microwave_id=${microwave_id}`);
+                    const data = await response.json();
+                    initializeReviews(data.reviews);
+                } catch (error) {
+                    console.error('Error fetching reviews:', error);
+                }
+            }
+        };
+
+        fetchData();
+    }, []);
+
+
+    //microwave stuff
+    const [microwaveName, setMicrowaveName] = useState();
+    const [microwaveDescrip, setMicrowaveDescrip] = useState();
+    useEffect(() => { 
+        fetchName();
+        fetchDescrip();
+    })
+
+    //fetch location and description to display
+    const fetchName = async () => {
+        try {
+            const response = await fetch(`http://localhost:3001/getMicrowaveName?microwave_id=${microwave_id}`);
+            const data = await response.json();
+
+            setMicrowaveName((microwaveName) => (
+                data.microwave_name));
+        } catch (error) {
+            console.error('Error fetching microwave name:', error);
+    }}; 
+
+    const fetchDescrip = async () => {
+        try {
+            const response = await fetch(`http://localhost:3001/getMicrowaveDescrip?microwave_id=${microwave_id}`);
+            const data = await response.json();
+
+            setMicrowaveDescrip((microwaveDescrip) => (
+                data.microwave_descrip));
+            console.log(microwaveDescrip)
+
+        } catch (error) {
+            console.error('Error fetching microwave name:', error);
+    }};    
+
+
+    //bring us to a new page with the form
+    const handleAdd = async () => {
+        window.location.href = `/new-review#${microwave_id}`;
+    };    
+
+    //return to dash
+    const handleClose = async () => {
+        window.location.href = `/dashboard`;
+    };  
+
+
+    return(
+            <div className="review-wrapper">
+                <div style={{textAlign:'center'}}>
+                    <h1>Microwave Reviews</h1>
+                    <h2>Location: {microwaveName}</h2>
+                    <h2>Description: {microwaveDescrip}</h2>
+                </div>
+                <Stack sx={{ pt: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', padding:'10px' }}>
+                    <Stack direction="row" spacing={4} justifyContent="center">
+                        <Button component="label" variant="contained" onClick={() => handleAdd()} >
+                            Add A Review?
+                        </Button>
+                        <Button type="submit" variant="contained" onClick={() => handleClose()} >
+                            Return to Reviews
+                        </Button>
+                    </Stack>
+                </Stack>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid container spacing={3} columns={{ xs: 12, sm: 6, md: 4 }} sx={{justifyContent:'center'}} >
+                        {reviews.map((review, index) => (
+                            <Grid item key={index}>
+                                <Card>
+                                    <CardActions sx={{justifyContent:'center'}}>
+                                        <Button startIcon={<StarRateIcon />}>
+                                            {`${review.rating || 0} Stars`}
+                                        </Button>
+                                    </CardActions>
+                                    <CardContent sx={{display:'block'}}>
+                                        <div className="review-text">
+                                            <p style={{fontWeight:'700', textAlign:'center'}}>User Wrote: </p>
+                                            <p style={{inlineSize:'350px', textAlign:'center', paddingLeft:'0px'}}>{review.review}</p>
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Box>  
+        </div>
+    );
+}
+    
+export default TestReviews;
