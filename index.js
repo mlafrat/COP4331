bca80a157f27518f167a15cac3858a66f4d3a813
Lomida <61120829+Lomida@users.noreply.github.com>
2023-11-25 20:04:06 -0500

    Another Test

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ connectToMongo()
     .then(database => {
         db = database;
 
+        app.use(cors());
+
         app.use(function(req, res, next) {
             res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
